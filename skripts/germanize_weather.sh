4b6d448483cdaea575a6e1e1210530e9ab77e5f7
kL3x <TowerPC.Tyre.Loc-DanielMeier@Saitow.AG>
2015-02-18 12:44:07 +0100

    erster upload

---
+++ skripts/germanize_weather.sh
@@ -0,0 +1,7 @@
+sed -i 's/Mon/Mo/g' ~/.cache/weather.xml 
+sed -i 's/Tue/Di/g' ~/.cache/weather.xml
+sed -i 's/Wed/Mi/g' ~/.cache/weather.xml
+sed -i 's/Thu/Do/g' ~/.cache/weather.xml
+sed -i 's/Fri/Fr/g' ~/.cache/weather.xml
+sed -i 's/Sat/Sa/g' ~/.cache/weather.xml
+sed -i 's/Sun/So/g' ~/.cache/weather.xml
